8079c3909a81e78f5313c9bb5f7eb8511694573f
Ian Fragoso <ianfragoso@Ians-MacBook-Pro.local>
2020-03-11 18:44:07 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const todoData = [
+    {
+        task: 'Organize Garage',
+        id: 1528817077286,
+        completed: false
+      },
+      {
+        task: 'Bake Cookies',
+        id: 1528817084358,
+        completed: false
+      }
+];
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
